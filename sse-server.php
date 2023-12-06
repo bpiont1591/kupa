65ac34b7c9661bb2f4b685fa194ad7158c73d81d
bpiont1591 <129689696+bpiont1591@users.noreply.github.com>
2023-12-06 19:36:37 +0100

    Update sse-server.php

--- sse-server.php
+++ sse-server.php
@@ -3,18 +3,24 @@ header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
 header('Connection: keep-alive');
 
-$onlineCount = 0;
-$messages = [];
+// Inicjalizacja danych, np. z bazy danych
+$onlineUsers = [];
 
 while (true) {
+    // Symulowane odczytywanie nowych wiadomości z bazy danych lub innego źródła
     $newMessages = ["Nowa wiadomość 1", "Nowa wiadomość 2"];
 
-    $onlineCount++;
+    // Symulowana zmiana online użytkowników
+    $onlineUsers = ["user1", "user2"];
 
-    echo "data: " . json_encode(["onlineCount" => $onlineCount, "newMessages" => $newMessages]) . "\n\n";
+    // Wyślij dane do klienta za pomocą SSE
+    echo "data: " . json_encode(["onlineUsers" => $onlineUsers, "newMessages" => $newMessages]) . "\n\n";
+
+    // Opróżnij bufor i wyślij dane do klienta
     ob_flush();
     flush();
 
+    // Symulowany interwał odświeżania (możesz dostosować)
     sleep(5);
 }
 ?>
